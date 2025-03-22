952d5250913d707077d32fc0c351c886b527b5e8
sirnik <113759817+sirnikLiK@users.noreply.github.com>
2025-03-23 01:49:57 +0300

    get_expresiion_img

--- try_text_founder.py
+++ try_text_founder.py
@@ -1,13 +1,36 @@
+# import opencv
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
-img = cv2.imread('expression.png')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#Преобразовываем изображение в оттенки серого
-_, img_black_white = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) #Преобразовываем изображение в черно-белое
+# Load the input image
+image = cv2.imread(r"C:\Users\eliza\Desktop\school\mathbot\expression.png")
+#cv2.imshow('Original', image)
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-config = r'--oem 3 --psm 6' 
 
-cv2.imshow('Result', img_black_white)
+
+def colored_mask(img, threshold = -1):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('gray.bmp', gray)
+
+
+    adaptiveThreshold = threshold if threshold >= 0 else cv2.mean(img)[0]
+    color = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    mask = cv2.inRange(color, (0, int(adaptiveThreshold / 6), 60), (180, adaptiveThreshold, 255))
+
+    # Создание маски цветной части изображения.
+    dst = cv2.bitwise_and(gray, gray, mask=mask)
+    cv2.imwrite('colors_mask.bmp', dst)
+    return dst
+
+
+
 cv2.waitKey(0)
+
+# Use the cvtColor() function to grayscale the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+colored_mask(image)
+cv2.imshow('Grayscale', gray_image)
+cv2.waitKey(0)  
+
+# Window shown waits for any key pressing event
+cv2.destroyAllWindows()
